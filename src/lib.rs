fbf3e29b7fe92bfcecfc036ff2a898c9cf8abb5f
j-devel <j@w3reality.com>
2021-10-01 18:02:47 +0700

    Expose `sys`, `ifce` and `psa_crypto`

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,13 @@ extern crate std;
 use mcu_if::{cstr_from, null_terminate_str, null_terminate_bytes};
 
 mod glue;
-mod sys;
+
+pub mod sys;
+
 pub mod r#if;
+pub use r#if as ifce;
+
+#[cfg(feature = "v3")]
+pub use psa_crypto;
+
 pub mod utils;
