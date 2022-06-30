e298a150217ac9a661c66d4ca4b8e30e18ef4d70
j-devel <38098238+j-devel@users.noreply.github.com>
2022-06-30 11:06:09 +0700

    Remove the legacy cross build indirection

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,6 @@ use mcu_if::{println, alloc::{vec, vec::Vec}, core2::io};
 
 use mcu_if::{cstr_from, null_terminate_str, null_terminate_bytes};
 
-#[cfg(feature = "v3")]
 pub use psa_crypto;
-
-#[cfg(feature = "v3")]
 pub mod psa_ifce;
-
 pub mod utils;
